f62b09836ffe2ad98959cc94758216bf4bacdebb
jonathan Diaz <jonatham01@hotmail.com>
2024-04-23 09:12:09 -0500

    authenticationManager created.

---
+++ Jwt-service/src/main/java/com/jwtservice/config/security/SecurityBeanInjector.java
@@ -0,0 +1,31 @@
+package com.jwtservice.config.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+
+@Configuration
+public class SecurityBeanInjector {
+
+    @Autowired
+    AuthenticationConfiguration authenticationConfiguration;
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(null);
+        authenticationProvider.setUserDetailsService(null);
+        return authenticationProvider;
+    }
+
+}
+
+
